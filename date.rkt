e09298da257ca6f25900cdc04cef9d993c9c4150
pwatrous <patrickwatrous@live.com>
2017-04-30 20:53:00 -0400

    finished!

--- date.rkt
+++ date.rkt
@@ -2,19 +2,6 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname date) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-; Converts day of week into its respective number
-(define (day-to-number day-of-week)
-  (cond [(equal? day-of-week "Monday") 0]
-        [(equal? day-of-week "Tuesday") 1]
-        [(equal? day-of-week "Wednesday") 2]
-        [(equal? day-of-week "Thursday") 3]
-        [(equal? day-of-week "Friday") 4]
-        [(equal? day-of-week "Saturday") 5]
-        [(equal? day-of-week "Sunday") 6]))
-
-(check-expect (day-to-number "Monday") 0)
-(check-expect (day-to-number "Wednesday") 2)
-
 ; Converts month in to its respective numerical code
 (define (month-to-number month)
   (cond [(equal? month "January") 6] ; becomes 5 on leap years
@@ -108,3 +95,57 @@
 (check-expect (after-2100-to-number 2109) 9)
 (check-expect (after-2100-to-number 2201) 4)
 (check-expect (after-2100-to-number 2301) 2)
+
+;; Calculates year code for any year
+(define (year-to-number year)
+  (cond [(and (>= year 2000) (< year 2100)) (21st-century-year-to-number year)]
+        [(>= year 2100) (after-2100-to-number year)]
+        [else (before-2000-to-number year)]))
+
+(check-expect (year-to-number 2100) 5)
+(check-expect (year-to-number 1999) 5)
+(check-expect (year-to-number 1900) 1)
+(check-expect (year-to-number 2200) 3)
+(check-expect (year-to-number 2009) 4)
+
+;; Mods a given number by 7
+(define (mod7 number)
+  (modulo number 7))
+
+(check-expect (mod7 6) 6)
+(check-expect (mod7 8) 1)
+(check-expect (mod7 (after-2100-to-number 2109)) 2)
+
+;; Calculates number from date
+(define (date-to-number month day year)
+  (cond [(and (is-leap-year? year) (equal? month "January"))
+         (mod7 (+ (- 1 day) (month-to-number month) (year-to-number year)))]
+        [(and (is-leap-year? year) (equal? month "February"))
+         (mod7 (+ (- 1 day) (month-to-number month) (year-to-number year)))]
+        [else (mod7 (+ day (month-to-number month) (year-to-number year)))]))
+
+(check-expect (date-to-number "January" 3 1999) 0)
+(check-expect (date-to-number "March" 18 2100) 4)
+(check-expect (date-to-number "October" 7 2301) 1)
+
+;; Converts number into final answer
+(define (date number)
+  (cond [(equal? number 0) "Sunday"]
+        [(equal? number 1) "Monday"]
+        [(equal? number 2) "Tuesday"]
+        [(equal? number 3) "Wednesday"]
+        [(equal? number 4) "Thursday"]
+        [(equal? number 5) "Friday"]
+        [(equal? number 6) "Saturday"]))
+
+(check-expect (date 0) "Sunday")
+(check-expect (date 2) "Tuesday")
+
+(define (main month day year)
+  (date (date-to-number month day year)))
+
+(check-expect (main "August" 6 1996) "Tuesday")
+(check-expect (main "April" 30 2017) "Sunday")
+(check-expect (main "October" 7 2301) "Monday")
+(check-expect (main "July" 4 1776) "Thursday")
+
